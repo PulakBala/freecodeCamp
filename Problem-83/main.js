b87cee2da7e0859627576c838f061c6f2f102d52
PulakBala <pulakbala0524@gmail.com>
2022-06-23 02:46:50 +0600

    seven commit

---
+++ Problem-83/main.js
@@ -0,0 +1,14 @@
+// Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+
+// You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array.
+
+const myDog = {
+    // Only change code below this line
+  name: "Pualk",
+  legs: 3,
+  tails: 5,
+  friends:["Nayon","Mamun"]
+  
+  
+    // Only change code above this line
+  };
