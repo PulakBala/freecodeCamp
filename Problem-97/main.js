cbdec81f02b94b542335e6aa835d6ece0a813739
PulakBala <pulakbala0524@gmail.com>
2022-06-23 22:04:08 +0600

    seven commit

---
+++ Problem-97/main.js
@@ -0,0 +1,12 @@
+// Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 0;
+while(i <= 5) {
+  myArray.push(i);
+  i++;
+}
+myArray.reverse();
